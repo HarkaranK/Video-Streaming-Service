d2e456c05132824b3bfe0752c65b1d856a24fa44
nmrtinn <hmedina6@my.bcit.ca>
2023-10-10 17:50:55 -0700

    please work

--- upload/app.py
+++ upload/app.py
@@ -1,18 +1,29 @@
-# Simplified version; integrate with authentication and database.
-from flask import Flask, request, redirect, url_for, jsonify
+from flask import Flask, request, render_template_string, jsonify
 import requests
 
 app = Flask(__name__)
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return "No file part"
-    file = request.files['file']
-
-    # Save to filesystem service
-    response = requests.post('http://filesystem:5000/upload', files={'file': file})
-    return response.json(), response.status_code
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "No file part"
+        file = request.files['file']
+        # Save to filesystem service
+        response = requests.post('http://filesystem:5002/upload', files={'file': file})
+        return response.json(), response.status_code
+    else:
+        # Render a basic upload form for GET request
+        return '''
+        <html>
+        <body>
+            <form method="post" enctype="multipart/form-data" action="/upload">
+                <input type="file" name="file">
+                <input type="submit" value="Upload">
+            </form>
+        </body>
+        </html>
+        '''
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
