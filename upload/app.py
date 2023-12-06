eeecc43c8f803fd91cb8a86973431ee1e0b0b6be
HarkarnK <hkang79@my.bcit.ca>
2023-12-06 00:58:27 -0800

    Fixed end points?

--- upload/app.py
+++ upload/app.py
@@ -36,7 +36,8 @@ def upload_file():
     # Send the file to the File System service
     files = {'file': (file.filename, file.stream, file.mimetype)}
     # response = requests.post('http://video-streaming-service-filesystem-1:5003/receive_file', files=files)
-    response = requests.post('http://my-flask-service-5003:5003/receive_file', files=files)
+    # response = requests.post('http://my-flask-service-5003:5003/receive_file', files=files)
+    response = requests.post('http://my-flask-service-5003.default.svc.cluster.local/receive_file', files=files)
 
 
     if response.status_code == 200:
@@ -44,7 +45,8 @@ def upload_file():
         db.session.add(video)
         db.session.commit()
         
-        return redirect('http://my-flask-service-5000:5000/videos-page')
+        # return redirect('http://my-flask-service-5000:5000/videos-page')
+        return redirect('http://my-flask-service-5000.default.svc.cluster.local/videos-page')
     else:
         return jsonify({'message': 'File upload failed'}), 500
 
